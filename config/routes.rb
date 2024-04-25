d6e8580f1ef305800d101030606418e06ec7bc4e
Ángel Díaz <diazangel9311@gmail.com>
2024-04-25 17:22:50 -0500

    Update devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Root path
-  root 'home#index'
+  root to: 'home#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
