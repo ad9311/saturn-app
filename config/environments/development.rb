2cd46789af5e59af4d53328177b766f8b8590c20
Ángel Díaz <diazangel9311@gmail.com>
2024-04-25 17:14:09 -0500

    Add home controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,8 +17,6 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  config.port = 3030
-
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
