78767e18bee7e120c578972cdde609e1239d7ac5
Ángel Díaz <diazangel9311@gmail.com>
2024-04-26 23:30:51 -0500

    Set budget_period for the budget_periods#show route

--- app/controllers/budget_periods_controller.rb
+++ app/controllers/budget_periods_controller.rb
@@ -1,7 +1,15 @@
 class BudgetPeriodsController < ApplicationController
+  before_action :set_budget_period, only: %i[show]
+
   def index
-    @budget_periods = current_user.budget_periods
+    @budget_periods = current_user.budget_periods.order(uid: :desc)
   end
 
   def show; end
+
+  private
+
+  def set_budget_period
+    @budget_period = BudgetPeriod.find_by(uid: params[:uid])
+  end
 end
