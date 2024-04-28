555adea0ab73d268ef5e32952368ccf421743af2
Ángel Díaz <diazangel9311@gmail.com>
2024-04-27 19:45:24 -0500

    Improve sliding menu js controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,10 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ['menu']
+  static targets = ['menu', 'menuContainer']
 
   toggleMenu() {
     this.menuTarget.classList.toggle('-left-full');
     this.menuTarget.classList.toggle('left-0');
+  }
+
+  connect() {
+    document.addEventListener('click', (event) => {
+      const clickOutside = !this.menuContainerTarget.contains(event.target);
+      if (clickOutside && this.menuTarget.classList.contains('left-0')) {
+        this.toggleMenu()
+      }
+    });
   }
 }
