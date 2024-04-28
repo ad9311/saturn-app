f1d2c12949bbea1357bd99edea11a92c18dd70eb
Ángel Díaz <diazangel9311@gmail.com>
2024-04-28 13:07:55 -0500

    Rename navbar stimulus controller to sliding menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def sliding_menu_links
+  def navigation_links
     [
       { label: 'Home', path: home_index_path },
       { label: 'Budgets', path: budget_periods_path }
--- app/javascript/controllers/sliding_menu_controller.js
+++ app/javascript/controllers/sliding_menu_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="navbar"
+// Connects to data-controller="sliding-menu"
 export default class extends Controller {
   static targets = ['menu', 'menuContainer']
 
