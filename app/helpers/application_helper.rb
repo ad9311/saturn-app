3e5b58ad59de1013cd0a2afd6cda194676fe1afd
Ángel Díaz <diazangel9311@gmail.com>
2024-04-29 22:27:50 -0500

    Implement settings styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       { label: 'Settings', path: user_settings_path }
     ]
   end
+
+  def settings_links
+    [
+      { body: 'Expense categories', path: expense_categories_path }
+    ]
+  end
 end
