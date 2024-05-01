e70e4af2238361ae352b53569eef33dee86b7f56
Ángel Díaz <diazangel9311@gmail.com>
2024-04-30 23:17:35 -0500

    Add vertical scrollbar to tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,14 @@ if Rails.env.development?
   ExpenseCategory.create(user:, name: 'Subscriptions', color: '#B60000')
   ExpenseCategory.create(user:, name: 'Other', color: '#B69500')
 
-  (1..12).each do |month|
-    budget_period = BudgetPeriod.create(user:, month:, year: 2023)
-    Faker::Number.between(from: 1, to: 10).times do
+  current_year = 2020
+  (1..36).each do |month_offset|
+    month = (month_offset % 12).zero? ? 12 : month_offset % 12
+    year = current_year + (month_offset / 12)
+
+    budget_period = BudgetPeriod.create(user:, month:, year:)
+
+    Faker::Number.between(from: 1, to: 50).times do
       if Faker::Number.between(from: 1, to: 10).even?
         IncomeTransaction.create(
           budget_period:,
