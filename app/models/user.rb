e0e3d5793f8d64240236a85749b579a6e187d87a
Ángel Díaz <diazangel9311@gmail.com>
2024-05-05 10:21:16 -0500

    Users cannot register

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise(
     :database_authenticatable,
-    :registerable,
+    # :registerable,
     :recoverable,
     :rememberable,
     :validatable,
