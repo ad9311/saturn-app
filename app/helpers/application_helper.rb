10b55a7dfc9820c207e8b9b39f3cd98771c828a3
Ángel Díaz <diazangel9311@gmail.com>
2024-05-05 10:04:09 -0500

    Implement locales for currency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,19 @@
 module ApplicationHelper
+  include ActionView::Helpers
+
   def navigation_links
     [
       { body: t('views.shared.navigation_links.home'), path: home_index_path },
       { body: t('views.shared.navigation_links.budget_periods'), path: budget_periods_path }
     ]
+  end
+
+  def currency(number)
+    number_to_currency(
+      number,
+      unit: t('views.shared.currency.unit'),
+      separator: t('views.shared.currency.separator'),
+      delimiter: t('views.shared.currency.delimiter')
+    )
   end
 end
