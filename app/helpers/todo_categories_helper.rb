bff08a4abb2dfb7c26af187c6a50e5cbf7bea924
Ángel Díaz <diazangel9311@gmail.com>
2024-05-10 17:51:04 -0500

    Fix errors in income form

--- app/helpers/todo_categories_helper.rb
+++ app/helpers/todo_categories_helper.rb
@@ -14,6 +14,14 @@ module TodoCategoriesHelper
     ]
   end
 
+  def todo_categories_edit_breadcrumbs(todo_list)
+    [
+      { body: todo_list.name, path: todo_list_path(todo_list) },
+      { body: t('views.todo_categories.title'), path: todo_list_todo_categories_path(todo_list) },
+      { body: t('views.todo_categories.navigiation_links.edit_category') }
+    ]
+  end
+
   def todo_categories_index_navigation_links(todo_list)
     [
       {
