2a1d3e3c191f070ee23fca4724dda8a41c7b2807
Ángel Díaz <diazangel9311@gmail.com>
2024-05-09 19:41:54 -0500

    Improve task row table

--- app/helpers/todo_lists_helper.rb
+++ app/helpers/todo_lists_helper.rb
@@ -1,8 +1,8 @@
 module TodoListsHelper
   def todo_list_full_tasks_table_rows(todo_list)
     rows = [
-      { text: t('views.todo_lists.table.done') },
       { text: t('views.todo_lists.table.edit') },
+      { text: t('views.todo_lists.table.done') },
       { text: t('views.todo_lists.table.task_description') },
       { text: t('views.todo_lists.table.priority'), skip: !todo_list.prioritized },
       { text: t('views.todo_lists.table.category'), skip: !todo_list.categorized },
