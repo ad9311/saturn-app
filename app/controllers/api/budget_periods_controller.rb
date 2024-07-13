75d6623fece4b54f1072ed2b29e441dd52f1951b
Ángel Díaz <diazangel9311@gmail.com>
2024-07-12 20:54:40 -0500

    Write method to include expenses and income params

--- app/controllers/api/budget_periods_controller.rb
+++ app/controllers/api/budget_periods_controller.rb
@@ -2,6 +2,7 @@ class Api::BudgetPeriodsController < ApplicationController
   before_action :set_budget_period, only: %i[show]
 
   include Api::ResponseBuilder
+  # include Api::BudgetPeriodsConcern
 
   def index
     # TODO: render errors
@@ -20,34 +21,33 @@ class Api::BudgetPeriodsController < ApplicationController
       render(json: build_response(data, status: :ERROR), status: :not_found) and return
     end
 
-    budget_period = @budget_period.serialized_hash
+    @budget_period_hash = @budget_period.serialized_hash
     include_params = params[:include]&.split(':') || []
+    expenses = include_params.any?('expenses')
+    income = include_params.any?('income')
 
-    if include_params.any?('expenses')
-      @expenses = @budget_period.expense_transactions
-                                .joins(:expense_category)
-                                .includes(:expense_category)
-                                .map(&:serialized_hash)
-    end
+    include_transactions(expenses, income)
 
-    @income = @budget_period.income_transactions.map(&:serialized_hash) if include_params.any?('income')
-
-    budget_period = budget_period.merge({ expenses: @expenses }) unless @expenses.nil?
-    budget_period = budget_period.merge({ income: @income }) unless @income.nil?
-
-    data = { budgetPeriod: budget_period }
+    data = { budgetPeriod: @budget_period_hash }
     render json: build_response(data, status: :SUCCESS)
   end
 
   def show_last
-    last_budget = current_user.budget_periods.order(:created_at).last
+    @budget_period = current_user.budget_periods.order(:created_at).last
 
-    if last_budget.nil?
+    if @budget_period.nil?
       data = { messages: ['this user does not have budget periods'] }
       render json: build_response(data, status: :SUCCESS) and return
     end
 
-    data = { budgetPeriod: last_budget.serialized_hash }
+    @budget_period_hash = @budget_period.serialized_hash
+    include_params = params[:include]&.split(':') || []
+    expenses = include_params.any?('expenses')
+    income = include_params.any?('income')
+
+    include_transactions(expenses, income)
+
+    data = { budgetPeriod: @budget_period_hash }
     render json: build_response(data, status: :SUCCESS)
   end
 
@@ -55,5 +55,16 @@ class Api::BudgetPeriodsController < ApplicationController
 
   def set_budget_period
     @budget_period = current_user.budget_periods.find_by(uid: params[:uid])
+  end
+
+  def include_transactions(expenses, income)
+    expenses_hash = expenses && @budget_period.expense_transactions
+                                              .joins(:expense_category)
+                                              .includes(:expense_category)
+                                              .map(&:serialized_hash)
+    income_hash = @budget_period.income_transactions.map(&:serialized_hash) if income
+
+    @budget_period_hash = @budget_period_hash.merge({ expenses: expenses_hash }) if expenses_hash
+    @budget_period_hash = @budget_period_hash.merge({ income: income_hash }) if income_hash
   end
 end
